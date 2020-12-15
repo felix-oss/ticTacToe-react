464af2b6da7d798b4531780ce8b6dab17e30313c
felix-oss <uv.mhjl@gmail.com>
2020-12-15 19:45:13 +0000

    update

--- src/js/component/board.js
+++ src/js/component/board.js
@@ -1,14 +1,15 @@
-// import React from "react";
+import React from "react";
 
-// class PlayBoard extends React.Component {
-// 	state = {
-// 		loading: true,
-// 		error: null
-// 	};
-
-// 	render() {
-// 		return <playBoardView {...this.state} />;
-// 	}
-// }
-
-// export default playBoard;
+export class PlayBoard extends React.Component {
+	render() {
+		return (
+			<div className="text-center mt-5">
+				<div className="board">
+					<div className="tile1" />
+					<div className="tile2" />
+					<div className="tile3" />
+				</div>
+			</div>
+		);
+	}
+}
